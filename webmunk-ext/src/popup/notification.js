f8f2cdf8ace5a19e736e7448e41ff57032bc94a5
fto2001 <jaldak14@gmail.com>
2024-08-14 12:31:05 +0300

    feat: rename id, move notification to another file

---
+++ webmunk-ext/src/popup/notification.js
@@ -0,0 +1,28 @@
+export class Notification {
+    constructor() {
+      this.notification = document.getElementById('notification-container');
+      this.notificationMessage = document.getElementById('notification-message');
+    }
+
+    show(message, duration = 3000, backgroundColor) {
+      this.notificationMessage.textContent = message;
+      this.notification.style.backgroundColor = backgroundColor;
+      this.notification.style.display = 'block';
+
+      setTimeout(() => {
+          this.notification.style.display = 'none';
+      }, duration);
+    }
+
+    error(message, duration) {
+      this.show(message, duration, '#f44336');
+    }
+
+    warning(message, duration) {
+      this.show(message, duration, '#ff9800');
+    }
+
+    info(message, duration) {
+      this.show(message, duration, '#2196F3');
+    }
+}
--- webmunk-ext/src/popup/popup.js
+++ webmunk-ext/src/popup/popup.js
@@ -1,4 +1,5 @@
 import { ENROLL_URL } from '../config';
+import { Notification } from './notification';
 
 class Popup {
   constructor() {
@@ -125,32 +126,3 @@ class Popup {
 }
 
 document.addEventListener('DOMContentLoaded', () => new Popup());
-
-class Notification {
-  constructor() {
-    this.notification = document.getElementById('notification');
-    this.notificationMessage = document.getElementById('notification-message');
-  }
-
-  show(message, duration = 3000, backgroundColor) {
-    this.notificationMessage.textContent = message;
-    this.notification.style.backgroundColor = backgroundColor;
-    this.notification.style.display = 'block';
-
-    setTimeout(() => {
-        this.notification.style.display = 'none';
-    }, duration);
-  }
-
-  error(message, duration) {
-    this.show(message, duration, '#f44336');
-  }
-
-  warning(message, duration) {
-    this.show(message, duration, '#ff9800');
-  }
-
-  info(message, duration) {
-    this.show(message, duration, '#2196F3');
-  }
-}
