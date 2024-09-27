2c9ed07f47a63a98c2648defc4772aea9dae5b30
fto2001 <jaldak14@gmail.com>
2024-09-27 14:07:22 +0300

    feature: make refactor

--- packages/modules/ad-personalization/src/content/strategies/FacebookCategoriesStrategy.ts
+++ packages/modules/ad-personalization/src/content/strategies/FacebookCategoriesStrategy.ts
@@ -7,7 +7,7 @@ export class FacebookCategoriesStrategy extends BaseStrategy {
   async execute() {
     const checkboxes = await this.waitForElements('input[type="checkbox"]');
 
-    if(!checkboxes) this.sendResponseToWorker(false, ErrorMessages.INVALID_URL);
+    if (!checkboxes) this.sendResponseToWorker(false, ErrorMessages.INVALID_URL);
 
     this.addBlurEffect();
 
