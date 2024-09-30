82a5023401ebcd37919238af326aeb0a15a924ce
fto2001 <jaldak14@gmail.com>
2024-09-30 13:38:04 +0300

    feature: add comment

--- packages/modules/extension-ads/src/content/content.js
+++ packages/modules/extension-ads/src/content/content.js
@@ -688,10 +688,11 @@ if ( typeof vAPI === 'object' && !vAPI.contentScript ) {
       if (node.localName === "iframe" && node.contentWindow && node.ownerDocument === document && node.getAttribute("role") !== "presentation") {
         const iframeSrc = node.src;
 
+        // We use this check to avoid tracking non-ad blocks from YouTube and Google by excluding their URLs.
         if (!iframeSrc.includes("youtube") && !iframeSrc.includes("google.com")) {
             this.highlightNodeAsAds(node, _indent, "red", "urlIsAnAd-hit", node.src);
         }
-     }
+      }
       if (node.nodeType === Node.ELEMENT_NODE) {
         for (const selector of this.cssSelectors) {
             if (node.localName === "iframe") {
