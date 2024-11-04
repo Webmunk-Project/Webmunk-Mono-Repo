b7e5cbf06700f050234fd766f143ac1e405fa676
Dima Smoliar <dimasmoliar@gmail.com>
2024-11-04 18:42:44 +0200

    Remove default user fetch interval.

--- webmunk-ext/src/config.js
+++ webmunk-ext/src/config.js
@@ -16,4 +16,4 @@ export const DELAY_BETWEEN_AD_PERSONALIZATION = process.env?.DELAY_BETWEEN_AD_PE
 // 1 hour
 export const REMOTE_CONFIG_FETCH_INTERVAL = process.env?.REMOTE_CONFIG_FETCH_INTERVAL;
 // 1 hour
-export const USER_FETCH_INTERVAL = process.env?.USER_FETCH_INTERVAL || 1000;
+export const USER_FETCH_INTERVAL = process.env?.USER_FETCH_INTERVAL;
