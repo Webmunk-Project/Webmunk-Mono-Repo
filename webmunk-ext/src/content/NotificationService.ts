edcfd8032bc0796c7b80c1a210529900dc4246e6
fto2001 <jaldak14@gmail.com>
2024-11-07 11:44:08 +0200

    feature: add new logo, remake notification banner

--- webmunk-ext/src/content/NotificationService.ts
+++ webmunk-ext/src/content/NotificationService.ts
@@ -95,7 +95,10 @@ export class NotificationService {
 
     const notificationContent = `
       <div style="display: flex; align-items: center; justify-content: space-between;">
-        <p style="font-size: 22px; font-weight: 700; color: black; margin: 0; line-height: 1.3;">Ad Study Notification</p>
+        <div style="display: flex; align-items: center; gap: 10px;">
+          <img style="width: 25px; height: 25px;" src="${chrome.runtime.getURL('images/favicon.png')}" alt="logo">
+          <p style="font-size: 22px; font-weight: 700; color: black; margin: 0; line-height: 1.3;">Ad Study Notification</p>
+        </div>
         <svg id="close-button" class="close-button" height="20px" viewBox="0 0 384 512">
           <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z">
           </path>
